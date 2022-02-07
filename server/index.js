c48a3c59d105ba3abe76e4fe76a100f86178cb42
Chris Petrevski <c_petrevski@hotmail.com>
2022-02-07 21:29:16 +1100

    deploying

--- server/index.js
+++ server/index.js
@@ -21,6 +21,14 @@ app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 // mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))).catch((error) => console.log(error.message));
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',

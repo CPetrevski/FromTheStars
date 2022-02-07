db9af2dba218f3e5660fc28e26634800a587f53d
Chris Petrevski <c_petrevski@hotmail.com>
2022-02-07 15:57:19 +1100

    deploying

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 8080;
 
 // mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))).catch((error) => console.log(error.message));
 mongoose.connect(
@@ -27,8 +27,6 @@ mongoose.connect(
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
     },
   );
 
